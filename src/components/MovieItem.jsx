a27658d87803ccecdf9b707b1d426bdcbd0cf67d
Yusuf-AI2011 <yusufshomurodov36@gmail.com>
2026-04-10 23:04:22 +0500

    done

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -6,7 +6,7 @@ export default class MovieItem extends Component {
     let image = "";
     data.Poster
       ? (image = data.Poster)
-      : (data = "https://commons.wikimedia.org/wiki/File:Image-not-found.png");
+      : (image = "https://commons.wikimedia.org/wiki/File:Image-not-found.png");
     return (
       <div className="w-50 flex flex-col justify-start items-center text-center gap-0.5 rounded-2xl border-2">
         <img className="w-50 h-70 rounded-2xl" src={image} alt={data.Title} />
